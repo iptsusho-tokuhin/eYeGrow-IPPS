03c6c7ae912bab0a0fba24b91e49518b2e9524d1
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2021-07-15 17:25:26 +0900

    Delete initial_javaf.js

--- initial_javaf.js
+++
@@ -1,7 +0,0 @@
-﻿var data_flag = location.search.substr(-1);
-
-if(data_flag == 1)
-{
-	var text = "eYeGrowのデモページへようこそ。\nこちらはeYeGrowの使い勝手を実際に体験していただける\nページとなっております。\n各ページの詳細については右下にいる「あいぴぃ君」を\nクリックしていただけると彼よりご説明いたします。\nそれではどうぞeYeGrowをご体験ください。";
-	swal(text);
-}
