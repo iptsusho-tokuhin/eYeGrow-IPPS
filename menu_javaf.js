559906463a2ad6884c2acfb5a920aba5f74cde0f
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2021-07-15 17:05:03 +0900

    Add files via upload

---
+++ menu_javaf.js
@@ -0,0 +1,11 @@
+﻿function Menu_Open()
+{
+	document.getElementById("blind_menu").style.visibility = "visible";
+	document.getElementById("side_menu").style.left = "0px";
+}
+
+function Menu_Close()
+{
+	document.getElementById("blind_menu").style.visibility = "hidden";
+	document.getElementById("side_menu").style.left = "-120px";
+}
