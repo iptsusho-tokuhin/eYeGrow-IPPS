bb8468d2b452cfbeef418600b3a34b7b04596391
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2021-07-26 14:23:34 +0900

    Update tutorial_settings_javaf.js

--- js/tutorial_settings_javaf.js
+++ js/tutorial_settings_javaf.js
@@ -6,7 +6,7 @@ Element[3] = document.getElementById("save");
 
 var ipkun = document.getElementById("ipkun");
 
-var Z = [1,2,3,4];
+var Z = [1,3,4,2];
 
 var Blind_Black = document.getElementById("blind_black");
 var Blind_Clea = document.getElementById("blind_clea");
